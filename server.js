bd68abb5a6f7880b166f1991b81abffaa6d3601f
Erni Shqerra <ershqerra04@gmail.com>
2025-11-03 14:03:20 -0500

    added user routes

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Someone made a request!');
-    res.render("index");
+    res.render("index", {user: "Erni!"});
 });
+
+app.get('/users', (req, res) => {
+    res.send('User list');
+});
+
+app.get('/users/new', (req, res) => {
+    res.send('New user form');
+});
+
+
 
 app.listen(3030);
 
