1591395ad394ccfcdb8f503f579fb9896943df1e
Erni Shqerra <ershqerra04@gmail.com>
2025-11-03 14:11:24 -0500

    created user router

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require('express');
-
+const userRouter = require('./routes/users');
 const app = express(); //Calling this function sets up a server
+
 app.set('view engine', 'ejs');
+app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     console.log('Someone made a request!');
     res.render("index", {user: "Erni!"});
 });
-
-app.get('/users', (req, res) => {
-    res.send('User list');
-});
-
-app.get('/users/new', (req, res) => {
-    res.send('New user form');
-});
-
 
 
 app.listen(3030);
