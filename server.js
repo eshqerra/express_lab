768b1f6ee5a65cbcc16d8fde07c0159d7ec03bfe
Erni Shqerra <ershqerra04@gmail.com>
2025-11-03 13:40:36 -0500

    added ejs and created first html page

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express(); //Calling this function sets up a server
-
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Someone made a request!');
-    res.send('Here!');
+    res.render("index");
 });
 
 app.listen(3030);
