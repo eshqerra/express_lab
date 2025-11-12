5c6661a9efa80d45cd0f719b94d518f193964880
Erni Shqerra <ershqerra04@gmail.com>
2025-11-11 22:12:55 -0500

    added posts routes and links in index,ejs

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = require('./routes/users');
+const postRouter = require('./routes/posts');
 const app = express(); //Calling this function sets up a server
 
 app.set('view engine', 'ejs');
 app.use('/users', userRouter);
+app.use('/postsLocation', postRouter);
 
 app.get('/', (req, res) => {
     console.log('Someone made a request!');
