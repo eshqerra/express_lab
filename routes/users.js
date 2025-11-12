0444b2bbb1844f75c88e2de278961a24e6dd6f7e
Erni Shqerra <ershqerra04@gmail.com>
2025-11-12 14:00:04 -0500

    added id param functiton

--- routes/users.js
+++ routes/users.js
@@ -25,4 +25,8 @@ router.route('/:id').get((req, res) => {
     res.send(`Updating User data: ${req.params.id}`);
 });
 
+router.param('id', (req, res, next, id) => {
+    console.log(`Accessing user: ${id}`);
+    next();
+});
 module.exports = router;
